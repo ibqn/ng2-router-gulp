69353851d48ab4c818e86be25e450933f57db67a
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-12-07 15:33:26 +0100

    protect loggedin variable to be accessed from outside

--- process/ts/auth.service.ts
+++ process/ts/auth.service.ts
@@ -5,16 +5,20 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
-    isLoggedIn: boolean = false;
+    private loggedIn: boolean = false;
 
     // store the URL so we can redirect after logging in
     redirectUrl: string;
 
     login(): Observable<boolean> {
-        return Observable.of(true).delay(1000).do(() => this.isLoggedIn = true);
+        return Observable.of(true).delay(1000).do(() => this.loggedIn = true);
     }
 
     logout(): void {
-        this.isLoggedIn = false;
+        this.loggedIn = false;
+    }
+
+    get isLoggedIn(): boolean {
+        return this.loggedIn;
     }
 }
