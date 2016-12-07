b82e2f190edf2be3a0f4c426229e9dd547e04682
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-12-07 15:16:32 +0100

    styles for admin component

--- process/ts/admin/admin.component.ts
+++ process/ts/admin/admin.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    templateUrl: 'admin.component.html'
+    templateUrl: 'admin.component.html',
+    styleUrls: ['admin.component.css']
 })
 export class AdminComponent {
 }
