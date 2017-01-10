6d52e21a802f66f3d8c44e1e4cd3c9c13eff808f
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-10 17:56:23 +0100

    update rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,35 @@
 //jshint esversion: 6
 
-import rollup      from 'rollup';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
 
 const entryPath = 'builds/release/';
+
+const warnFilter = /The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten/;
+
+// taken from https://github.com/rollup/rollup/blob/master/bin/src/runRollup.js
+import chalk from 'chalk';
+const seen = new Set();
+if (!process.stderr.isTTY) chalk.enabled = false;
+const warnSymbol = process.stderr.isTTY ? `⚠️   ` : `Warning: `;
+const stderr = console.error.bind(console);
+const handleWarning = warning => {
+    const str = warning.toString();
+    if (seen.has(str)) return;
+    seen.add(str);
+    stderr(`${warnSymbol}${chalk.bold( warning.message )}`);
+    if (warning.url) {
+        stderr(chalk.cyan(warning.url));
+    }
+    if (warning.loc) {
+        stderr(`${warning.loc.file} (${warning.loc.line}:${warning.loc.column})`);
+    }
+    if (warning.frame) {
+        stderr(chalk.dim(warning.frame));
+    }
+    stderr('');
+};
 
 //paths are relative to the execution path
 export default {
@@ -15,6 +39,11 @@ export default {
     //sourceMap: true,
     //sourceMapFile: entryPath + 'js/build.js.map',
     format: 'iife',
+    onwarn: warning => {  // overwite the default warning function
+        const str = warning.toString();
+        if (warnFilter.test(str)) return;
+        handleWarning(warning);
+    },
     plugins: [
         nodeResolve({
             jsnext: true,
