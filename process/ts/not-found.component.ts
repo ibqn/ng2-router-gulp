3f4d865c74ae59ca521495942cb5bcc2365ee4cd
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-02-28 12:26:06 +0100

    change template for the 404 page

--- process/ts/not-found.component.ts
+++ process/ts/not-found.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    template: '<h2>Page not found</h2>'
+    template: '<h2>404 - Page not found.</h2>'
 })
 export class PageNotFoundComponent {}
